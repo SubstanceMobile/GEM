5d15ba3652533ff253af415c9c698229b14fc464
Animbus <adrianvovk@gmail.com>
2015-06-27 20:59:27 -0400

    In Between PC's

--- mobile/src/main/java/com/animbus/music/media/MusicService.java
+++ mobile/src/main/java/com/animbus/music/media/MusicService.java
@@ -8,13 +8,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.session.MediaSession;
 import android.media.session.PlaybackState;
-import android.support.v4.media.session.MediaSessionCompat;
 import android.util.Log;
 
 import com.animbus.music.NowPlayingClassic;
+import com.animbus.music.R;
 import com.animbus.music.activities.NowPlaying;
 import com.animbus.music.data.SettingsManager;
-import com.animbus.music.data.dataModels.Song;
 
 import java.util.Collections;
 import java.util.List;
@@ -60,7 +59,7 @@ public class MusicService extends Service implements PlaybackManager.Callback {
     /**
      * The queue (see {@link com.animbus.music.data.dataModels.Song})
      */
-    private List<Song> mPlayingQueue;
+    private List<MediaSession.QueueItem> mPlayingQueue;
     /**
      * This is the position of the playing song in the queue
      */
@@ -74,10 +73,14 @@ public class MusicService extends Service implements PlaybackManager.Callback {
     private DelayedStopHandler mDelayedStopHandler = new DelayedStopHandler(this);
     private PlaybackManager mPlayback;
 
+    private boolean doRepeat;
+    private boolean doShuffle;
+
     /**
-      * (non-Javadoc)
-      * @see android.app.Service#onCreate()
-      */
+     * (non-Javadoc)
+     *
+     * @see android.app.Service#onCreate()
+     */
     @Override
     public void onCreate() {
         super.onCreate();
@@ -114,6 +117,7 @@ public class MusicService extends Service implements PlaybackManager.Callback {
 
     /**
      * (non-Javadoc)
+     *
      * @see android.app.Service#onStartCommand(android.content.Intent, int, int)
      */
     @Override
@@ -132,4 +136,65 @@ public class MusicService extends Service implements PlaybackManager.Callback {
         return START_STICKY;
     }
 
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy");
+
+        //Will release resources
+        handleStopRequest(null);
+        mDelayedStopHandler.removeCallbacksAndMessages(null);
+        //Release media session and clear up resources
+        mSession.release();
+    }
+
+    private final class MediaSessionCallback extends MediaSession.Callback {
+
+        @Override
+        public void onPlay() {
+            Log.d(TAG, "onPlay");
+
+            if (mPlayingQueue != null && !mPlayingQueue.isEmpty()) {
+                mSession.setQueue(mPlayingQueue);
+                mSession.setQueueTitle(getString(R.string.title_queue));
+            } else {
+                Log.e(TAG, "The queue is empty");
+            }
+
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+        }
+
+        @Override
+        public void onSkipToNext() {
+            if (doRepeat) {
+                handlePlayRequest(getCurrentSong());
+            } else {
+                handlePlayRequest(getNextSong());
+            }
+        }
+
+        @Override
+        public void onSkipToPrevious() {
+
+        }
+
+        @Override
+        public void onSkipToQueueItem(long id) {
+
+        }
+
+        @Override
+        public void onSeekTo(long pos) {
+
+        }
+
+        public void handlePlayRequest() {}
+
+        public void handlePlayRequest(List<MediaSession.QueueItem> list, int pos) {}
+
+        public void handlePlayRequest(MediaSession.QueueItem song)
+    }
 }
