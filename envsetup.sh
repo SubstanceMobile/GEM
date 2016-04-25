6189389e2a6a10eb786569260a35c0e1e72c8989
AnimbusDev <avovk@substanceproject.net>
2016-04-24 20:01:24 -0400

    Windows scripts complete

--- envsetup.sh
+++ envsetup.sh
@@ -9,13 +9,14 @@ echo "gemapp() { cd" $GEMDIR";}" >> ~/.bashrc
 
 echo "Keystore setup"
 read -e -p "Path of your keystore file: " STOREFILE
-read -e -s -p "Keystore Password: " STOREPASS
+read -e -s -p "Keystore password: " STOREPASS
 echo
 read -e -p "Key alias: " KEYALIAS
 read -e -s -p "Key password: " KEYPASS
 echo
 
 echo "Creating Gradle files"
+rm mobile/gradle.properties
 touch mobile/gradle.properties
 echo "STOREFILE = "$STOREFILE >> mobile/gradle.properties
 echo "" >> mobile/gradle.properties
@@ -24,9 +25,6 @@ echo "" >> mobile/gradle.properties
 echo "KEYALIAS = "$KEYALIAS >> mobile/gradle.properties
 echo "" >> mobile/gradle.properties
 echo "KEYPASS = "$KEYPASS >> mobile/gradle.properties
-
-echo "Updating file permissions"
-chmod +x scripts/linux/adbtool
 
 read -p "Modify PATH? (y/n): " -n 1 GO
 echo
